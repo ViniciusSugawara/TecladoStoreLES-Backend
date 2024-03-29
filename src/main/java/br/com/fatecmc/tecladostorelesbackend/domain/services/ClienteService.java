8c3668e87805688f6825210c2418a7b095d77e83
Vini <vinicius.sugawara@newgo.com.br>
2024-03-29 20:25:25 -0300

    feat: adicionado metodos para validar maior tamanho de id de endereço e cartão, alem de metodos para retornar especificamente os endereços e cartões do cliente

--- src/main/java/br/com/fatecmc/tecladostorelesbackend/domain/services/ClienteService.java
+++ src/main/java/br/com/fatecmc/tecladostorelesbackend/domain/services/ClienteService.java
@@ -1,5 +1,7 @@
 package br.com.fatecmc.tecladostorelesbackend.domain.services;
 
+import br.com.fatecmc.tecladostorelesbackend.data.repositories.CartaoRepository;
+import br.com.fatecmc.tecladostorelesbackend.data.repositories.EnderecoRepository;
 import br.com.fatecmc.tecladostorelesbackend.domain.models.cliente.Cartao;
 import br.com.fatecmc.tecladostorelesbackend.domain.models.cliente.Cliente;
 import br.com.fatecmc.tecladostorelesbackend.domain.models.cliente.Endereco;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.modelmapper.ModelMapper;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -23,9 +26,13 @@ import java.util.stream.Collectors;
 @Service
 public class ClienteService {
     private ClienteRepository clienteRepository;
+    private EnderecoRepository enderecoRepository;
+    private CartaoRepository cartaoRepository;
     private ModelMapper mapper;
-    public ClienteService(ClienteRepository clienteRepository) {
+    public ClienteService(ClienteRepository clienteRepository, EnderecoRepository enderecoRepository, CartaoRepository cartaoRepository) {
         this.clienteRepository = clienteRepository;
+        this.enderecoRepository = enderecoRepository;
+        this.cartaoRepository = cartaoRepository;
         this.mapper = new ModelMapper();
     }
 
@@ -65,6 +72,45 @@ public class ClienteService {
         return clienteRetorno;
     }
 
+    public Endereco findEnderecoOnCliente(Long idCliente, Long idEndereco){
+        return clienteRepository.findById(idCliente)
+                .get()
+                .getEnderecos()
+                .stream()
+                .filter(endereco1 -> endereco1.getId() == idEndereco)
+                .findFirst()
+                .get();
+    }
+
+    public Set<EnderecoDTO> findEnderecosByClienteId(Long idCliente){
+        Cliente cliente = this.clienteRepository.findById(idCliente).get();
+
+        Set<EnderecoDTO> enderecosMapeados = new HashSet<>();
+        cliente.getEnderecos()
+                .stream()
+                .forEach(endereco -> {
+            EnderecoDTO enderecomap = mapper.map(endereco, EnderecoDTO.class);
+            enderecosMapeados.add(enderecomap);
+        });
+
+        return enderecosMapeados;
+    }
+
+
+    public Set<CartaoDTO> findCartoesByClienteId(Long idCliente) {
+        Cliente cliente = this.clienteRepository.findById(idCliente).get();
+
+        Set<CartaoDTO> cartoesMapeados = new HashSet<>();
+        cliente.getCartoesCredito()
+                .stream()
+                .forEach(cartao -> {
+                    CartaoDTO cartaomap = mapper.map(cartao, CartaoDTO.class);
+                    cartoesMapeados.add(cartaomap);
+                });
+
+        return cartoesMapeados;
+    }
+
     public ClienteRetornoDTO save(ClienteCadastroDTO cliente){
         if(senhaInvalida(cliente.getSenha())){
             throw new RuntimeException("Senha invalida");
@@ -80,12 +126,6 @@ public class ClienteService {
         return clienteRetorno;
     }
 
-    private boolean senhaInvalida(String senha){
-        String regex = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(senha);
-        return matcher.matches();
-    }
 
     public ClienteRetornoDTO addEndereco(Long idCliente, EnderecoDTO endereco){
         verifyById(idCliente);
@@ -142,11 +182,7 @@ public class ClienteService {
         mapper.getConfiguration().setSkipNullEnabled(true);
 
         Cliente clienteRetornado = this.clienteRepository.findById(idCliente).get();
-        Endereco enderecoEncontrado = clienteRetornado.getEnderecos()
-                .stream()
-                .filter(endereco1 -> endereco1.getId() == idEndereco)
-                .findFirst()
-                .get();
+        Endereco enderecoEncontrado = findEnderecoOnCliente(idCliente, idEndereco);
         mapper.map(endereco, enderecoEncontrado);
         clienteRetornado.getEnderecos().add(enderecoEncontrado);
 
@@ -172,17 +208,35 @@ public class ClienteService {
         this.clienteRepository.deleteById(idCliente);
     }
 
+    public Long verifyMaxAddressId(){
+        return this.enderecoRepository.findAll()
+                .stream()
+                .mapToLong(Endereco::getId)
+                .max().orElse(0l) + 1;
+    }
+    public Long verifyMaxPaymentId(){
+        return this.cartaoRepository.findAll()
+                .stream()
+                .mapToLong(Cartao::getId)
+                .max().orElse(0l) + 1;
+    }
     private void verifyById(Long id){
         if(!this.clienteRepository.existsById(id)) {
             throw new RuntimeException("Id não existe");
         }
     }
-
     private Set<Long> convertEnderecosToId(Cliente cliente){
         return cliente.getEnderecos().stream().map(endereco -> endereco.getId()).collect(Collectors.toSet());
     }
     private Set<Long> convertCartoesToId(Cliente cliente){
         return cliente.getCartoesCredito().stream().map(cartao -> cartao.getId()).collect(Collectors.toSet());
+    }
+
+    private boolean senhaInvalida(String senha){
+        String regex = "^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(senha);
+        return matcher.matches();
     }
 
 }
